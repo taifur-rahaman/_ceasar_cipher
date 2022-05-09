48fb99f8efa364c2bff790c39d6f182fc182c860
Taifur Rahaman <tsar_nosferatu@Taifurs-MacBook-Air.local>
2022-05-09 19:39:00 +0600

    v1.0.0 completed

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
+def shifting(shifted_index):
+    if shifted_index > len(alphabet):
+        shifted_index = abs(shifted_index - len(alphabet))
+
+
 def encrypt(text, shift):
     encrypted_char = []
     for char in text:
         shifted_index = alphabet.index(char) + shift
-        if shifted_index > len(alphabet):
-            shifted_index = abs(shifted_index - len(alphabet))
+        shifting(shifted_index)
         encrypted_char.append(alphabet[shifted_index])
     return ''.join(encrypted_char)
+
+
+def decrypt(text, shift):
+    decrypted_char = []
+    for char in text:
+        shifted_index = alphabet.index(char) - shift
+        shifting(shifted_index)
+        decrypted_char.append(alphabet[shifted_index])
+    return ''.join(decrypted_char)
 
 
 while True:
@@ -24,4 +37,15 @@ while True:
         print(f"Your encrypted text is '{encrypted_text}'")
 
     elif choice == 2:
-        pass
+        text = input("Enter the text to decrypt: ")
+        shift = int(input("Enter the shift: "))
+        decrypted_text = decrypt(text, shift)
+        print(f"Your decrypted text is '{decrypted_text}'")
+
+    elif choice == 0:
+        print("Thank you for Using Ceasar Chipher")
+        exit()
+
+    else:
+        print("INVALID choice. Please try again")
+        continue
